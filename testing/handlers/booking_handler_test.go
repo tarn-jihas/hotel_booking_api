40a65f77e7318cf5fced351bc7370a396d23345c
Strahinja <43478545+keselj-strahinja@users.noreply.github.com>
2023-05-28 19:46:27 +0200

    finished booking testing

--- testing/handlers/booking_handler_test.go
+++ testing/handlers/booking_handler_test.go
@@ -20,15 +20,16 @@ func TestUserGetBooking(t *testing.T) {
 
 	defer db.Teardown()
 	var (
-		user    = fixtures.AddUser(db.Store, "james", "foo", false)
-		hotel   = fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
-		room    = fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
-		from    = time.Now()
-		till    = from.AddDate(0, 0, 5)
-		booking = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
-		app     = fiber.New()
-		route   = app.Group("/:id", middleware.JWTAuthentication(db.User))
-		_       = booking
+		nonAuthUser = fixtures.AddUser(db.Store, "jimmy", "coolboi", false)
+		user        = fixtures.AddUser(db.Store, "james", "foo", false)
+		hotel       = fixtures.AddHotel(db.Store, "bar hotel", "a", 4, nil)
+		room        = fixtures.AddRoom(db.Store, "small", true, 4.4, hotel.ID)
+		from        = time.Now()
+		till        = from.AddDate(0, 0, 5)
+		booking     = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+		app         = fiber.New()
+		route       = app.Group("/:id", middleware.JWTAuthentication(db.User))
+		_           = booking
 
 		bookinHandler = api.NewBookingHandler(db.Store)
 	)
@@ -46,6 +47,28 @@ func TestUserGetBooking(t *testing.T) {
 
 	var bookingResp *types.Booking
 	if err := json.NewDecoder(resp.Body).Decode(&bookingResp); err != nil {
+		t.Fatal(err)
+	}
+
+	if bookingResp.ID != booking.ID {
+		t.Fatalf("expected  %s got %s", booking.ID, bookingResp.ID)
+	}
+
+	if bookingResp.UserID != booking.UserID {
+		t.Fatalf("expected  %s got %s", booking.UserID, bookingResp.UserID)
+	}
+
+	// test non-admin cannot access the bookings
+	token = api.CreateTokenFromUser(nonAuthUser)
+
+	req = httptest.NewRequest("GET", "/"+booking.ID.Hex(), nil)
+	req.Header.Add("x-api-token", token)
+	resp, err = app.Test(req)
+
+	if resp.StatusCode == http.StatusOK {
+		t.Fatalf("expected a non 200 status code but got this: %d", resp.StatusCode)
+	}
+	if err != nil {
 		t.Fatal(err)
 	}
 
@@ -95,6 +118,10 @@ func TestAdminGetBookings(t *testing.T) {
 		t.Fatalf("expected 1 booking got %d", len(bookings))
 	}
 	have := bookings[0]
+
+	if have.ID != booking.ID {
+		t.Fatalf("expected  %s got %s", booking.ID, have.ID)
+	}
 
 	if have.UserID != booking.UserID {
 		t.Fatalf("expected  %s got %s", booking.UserID, have.UserID)
